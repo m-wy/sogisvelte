54f535ca44ed72874639ac00773bdb82233cfd36
m-wy <matt.willay@gmail.com>
2020-10-26 10:12:40 +0100

    Ajout support scss

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,6 @@
+<script>
+	import App from "../node_modules/@sapper/internal/App.svelte";
+</script>
 <svelte:head>
 	<title>About</title>
 </svelte:head>
@@ -5,3 +8,14 @@
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+<button on:click>Click me</button>
+
+<style lang="scss">
+
+	$primary: red;
+
+	button {
+		color: $primary;
+	}
+</style>
+
